2116d4918f6a6fd9b98f95afcb09384dd8d583e6
goizane <goizanealonso95@gmail.com>
2019-04-09 22:42:03 +0200

    Semana 11

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var cacheVersion = 1;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = 'juego.html';
+
+
+function createCacheBustedRequest(url) {
+	let request = new Request(url, {cache: 'reload'});
+	if ('cache' in request) {
+		return request;
+	}
+	let bustedUrl = new URL(url, self.location.href);
+	bustedUrl.search += (bustedUrl.search ? '&' : '') + 'cachebust=' + Date.now();
+	return new Request(bustedUrl);
+}
+
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+          offlineUrl
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  // request.mode = navigate isn't supported in all browsers
+  // so include a check for Accept: text/html header.
+  console.log("No tienes conexión a internet!");
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              // Return the offline page
+              return caches.match(offlineUrl);
+          })
+    );
+  }
+  else{
+        // Respond with everything else if we can
+        event.respondWith(caches.match(event.request)
+                        .then(function (response) {
+                        return response || fetch(event.request);
+                    })
+            );
+      }
+});
